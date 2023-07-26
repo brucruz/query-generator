8e4c61f6597329b16b7b3797ee8efa117d1af798
Bruno Cruz <bru.cruz@gmail.com>
2023-07-26 14:35:04 -0300

    feat: added created by

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,26 +6,39 @@ export default function Home() {
     <main className="flex h-screen w-full bg-gray-900">
       <SchemaSidebar />
 
-      <div className="flex-1 flex flex-col items-center w-full px-12 pt-24 py-16 h-screen space-between">
-        <div className="max-w-2xl w-full">
-          <h1 className="text-center text-2xl font-bold text-slate-100 pb-2">
-            SQL Query Generator
-          </h1>
-          <h2 className="text-center text-lg text-slate-100 pb-8">
-            Generate SQL queries with AI
-          </h2>
+      <div className="flex-1 flex flex-col items-center w-full h-screen">
+        <div className="flex-1 flex flex-col items-center w-full px-12 pt-24 py-16 pb-8 h-full space-between">
+          <div className="max-w-2xl w-full">
+            <h1 className="text-center text-2xl font-bold text-slate-100 pb-2">
+              SQL Query Generator
+            </h1>
+            <h2 className="text-center text-lg text-slate-100 pb-8">
+              Generate SQL queries with AI
+            </h2>
 
-          <h3 className="text-left text-md font-semibold text-slate-100">
-            What do you want to query?
-          </h3>
+            <h3 className="text-left text-md font-semibold text-slate-100">
+              What do you want to query?
+            </h3>
 
-          <p className="pt-4 text-sm">
-            Describe to the AI assistant what you want to achieve with your
-            database and it will return the SQL
-          </p>
+            <p className="pt-4 text-sm">
+              Describe to the AI assistant what you want to achieve with your
+              database and it will return the SQL
+            </p>
+          </div>
+
+          <Chat />
         </div>
 
-        <Chat />
+        <span className="text-xs pb-4">
+          Created by{" "}
+          <a
+            className="underline underline-offset-4"
+            href="https://github.com/brucruz/query-generator"
+            target="_blank"
+          >
+            Bruno Cruz
+          </a>
+        </span>
       </div>
     </main>
   );
