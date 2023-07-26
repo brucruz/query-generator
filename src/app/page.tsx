eb5351e725800e0a5979bcca341510ea32aabcc7
Bruno Cruz <bru.cruz@gmail.com>
2023-07-26 12:20:18 -0300

    feat: implement SchemaSidebar - added click to show example functionality - saving schema to local storage - retrieving schema from local storage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,96 +1,16 @@
 "use client"; // This is a client component 👈🏽
 
 import Button from "@/components/Button";
+import SchemaSidebar from "@/components/SchemaSidebar";
 import TextArea from "@/components/TextArea";
 import { useChat } from "ai/react";
 
 export default function Home() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
 
-  const example = `
-  CREATE TABLE Orders (
-    OrderID int,
-    CustomerID int,
-    OrderDate datetime,
-    OrderTime varchar(8),
-    PRIMARY KEY (OrderID)
-  );
-
-  CREATE TABLE OrderDetails (
-    OrderDetailID int,
-    OrderID int,
-    ProductID int,
-    Quantity int,
-    PRIMARY KEY (OrderDetailID)
-  );
-
-  CREATE TABLE Products (
-    ProductID int,
-    ProductName varchar(50),
-    Category varchar(50),
-    UnitPrice decimal(10, 2),
-    Stock int,
-    PRIMARY KEY (ProductID)
-  );
-
-  CREATE TABLE Customers (
-    CustomerID int,
-    FirstName varchar(50),
-    LastName varchar(50),
-    Email varchar(100),
-    Phone varchar(20),
-    PRIMARY KEY (CustomerID)
-  );`;
-
   return (
     <main className="flex h-screen w-full bg-gray-900">
-      <aside className="flex-none border-solid border-r px-12 py-24 h-full bg-gray-800 border-gray-700 max-w-lg overflow-y-auto">
-        <h2 className="text-left text-lg font-semibold text-slate-100">
-          How it works?
-        </h2>
-
-        <ol className="py-8">
-          <li className="list-decimal list-inside pb-2 text-sm">
-            Enter your schema and save it
-          </li>
-          <li className="list-decimal list-inside pb-2 text-sm">
-            Describe what you want to query to the AI assistant and it will
-            generate it for you
-          </li>
-          <li className="list-decimal list-inside text-sm">
-            Copy the query and use it in your project
-          </li>
-        </ol>
-
-        <form className="max-w-md" onSubmit={() => {}}>
-          <h3 className="text-left text-md font-semibold text-slate-100 pt-4">
-            Database Schema:
-          </h3>
-
-          <TextArea
-            id="user-input"
-            value=""
-            rows={12}
-            placeholder="What is your database schema?"
-            onChange={() => {}}
-          />
-          <Button type="submit" text="Save schema" />
-
-          <span className="block text-sm underline underline-offset-4 w-fit mx-auto text-center py-4 cursor-pointer">
-            Show Example
-          </span>
-        </form>
-
-        <div className="pt-8">
-          <h3 className="text-left text-md font-semibold text-slate-100">
-            Current schema:
-          </h3>
-
-          <div className="py-4 whitespace-break-spaces">
-            <p className="text-slate-100 text-sm">{example}</p>
-          </div>
-        </div>
-      </aside>
+      <SchemaSidebar />
 
       <div className="flex-1 flex flex-col items-center w-full px-12 pt-24 py-16 h-screen space-between">
         <div className="max-w-2xl w-full">
---
+++ src/components/SchemaSidebar.tsx
@@ -0,0 +1,119 @@
+import { useEffect, useState } from "react";
+import Button from "./Button";
+import TextArea from "./TextArea";
+
+const example = `CREATE TABLE Orders (
+  OrderID int,
+  CustomerID int,
+  OrderDate datetime,
+  OrderTime varchar(8),
+  PRIMARY KEY (OrderID)
+);
+
+CREATE TABLE OrderDetails (
+  OrderDetailID int,
+  OrderID int,
+  ProductID int,
+  Quantity int,
+  PRIMARY KEY (OrderDetailID)
+);
+
+CREATE TABLE Products (
+  ProductID int,
+  ProductName varchar(50),
+  Category varchar(50),
+  UnitPrice decimal(10, 2),
+  Stock int,
+  PRIMARY KEY (ProductID)
+);
+
+CREATE TABLE Customers (
+  CustomerID int,
+  FirstName varchar(50),
+  LastName varchar(50),
+  Email varchar(100),
+  Phone varchar(20),
+  PRIMARY KEY (CustomerID)
+);`;
+
+export function SchemaSidebar(): JSX.Element {
+  const [schema, setSchema] = useState<string | null>(null);
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    const storedSchema = localStorage.getItem("schema");
+
+    if (storedSchema) {
+      setSchema(storedSchema);
+    }
+  }, []);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    saveSchema(value);
+  };
+
+  const saveSchema = (input: string) => {
+    setSchema(input);
+    // save schema to local storage
+    localStorage.setItem("schema", input);
+  };
+
+  return (
+    <aside className="flex-none border-solid border-r px-12 py-24 h-full bg-gray-800 border-gray-700 max-w-lg overflow-y-auto">
+      <h2 className="text-left text-lg font-semibold text-slate-100">
+        How it works?
+      </h2>
+
+      <ol className="py-8">
+        <li className="list-decimal list-inside pb-2 text-sm">
+          Enter your schema and save it
+        </li>
+        <li className="list-decimal list-inside pb-2 text-sm">
+          Describe what you want to query to the AI assistant and it will
+          generate it for you
+        </li>
+        <li className="list-decimal list-inside text-sm">
+          Copy the query and use it in your project
+        </li>
+      </ol>
+
+      <form className="max-w-md" onSubmit={handleSubmit}>
+        <h3 className="text-left text-md font-semibold text-slate-100 pt-4">
+          Database Schema:
+        </h3>
+
+        <TextArea
+          id="user-input"
+          value={value}
+          rows={12}
+          placeholder="What is your database schema?"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <Button type="submit" text="Save schema" />
+
+        <span
+          className="block text-sm underline underline-offset-4 w-fit mx-auto text-center py-4 cursor-pointer"
+          onClick={() => setValue(example)}
+        >
+          Show Example
+        </span>
+      </form>
+
+      <div className="pt-8">
+        <h3 className="text-left text-md font-semibold text-slate-100">
+          Current schema:
+        </h3>
+
+        <div className="py-4 whitespace-break-spaces">
+          <p className="text-slate-100 text-sm">
+            {schema ? schema : "No schema saved yet"}
+          </p>
+        </div>
+      </div>
+    </aside>
+  );
+}
+
+export default SchemaSidebar;
